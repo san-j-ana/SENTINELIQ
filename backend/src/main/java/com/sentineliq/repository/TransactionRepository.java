1d61b2e77ed0db556c95215b8703cb21c0883d1a
Sanjanak <k10sanjana@gmail.com>
2026-08-02 02:29:17 +0530

    Create TransactionRepository.java

---
+++ backend/src/main/java/com/sentineliq/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.sentineliq.repository;
+
+import com.sentineliq.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
