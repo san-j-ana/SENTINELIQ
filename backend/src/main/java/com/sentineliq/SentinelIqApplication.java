9f115739fb8c008069ebfa97ea867a9ec95d761b
Sanjanak <k10sanjana@gmail.com>
2026-08-02 02:25:58 +0530

    Create SentinelIqApplication.java

---
+++ backend/src/main/java/com/sentineliq/SentinelIqApplication.java
@@ -0,0 +1,13 @@
+package com.sentineliq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelIqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelIqApplication.class, args);
+    }
+
+}
